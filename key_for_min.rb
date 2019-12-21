589b0b6de66889a0229029a657ab866b7ca3f9e5
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2019-12-21 21:22:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  if name_hash == {}
+  if name_hash == {} #if hash is empty then return nil
     return nil
   else
     lowValue = name_hash.first[1] #set lowest value to hash first value
