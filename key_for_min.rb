317dd0a3b1645e0759997c72ff147479b4741c80
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2019-12-21 21:22:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,11 +7,11 @@ def key_for_min_value(name_hash)
   else
     lowValue = name_hash.first[1] #set lowest value to hash first value
     lowKey = name_hash.first[0] #set key to the lowest value to first position in hash
-    name_hash.each{|key, val| #for each key and value do as follows under
-      if val < lowValue
-        lowValue = val
-        lowKey = key
-      end}
+    name_hash.each do |key, val| #for each key and value do as follows under
+      if val < lowValue #if value is less than current lowest value, do under
+        lowValue = val  #set lowest value to val
+        lowKey = key  #set lowest key to key
+      end
     end
     return lowKey
     
