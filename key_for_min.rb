5bf48711e488c58b3c81b9e1e92b0edff23ffaf7
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2019-12-21 21:08:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  if 
+  if test == {}
+       return nil
+  else
+       test = test.sort_by {|k, v| v}
+       test[0]
+  end
 end
