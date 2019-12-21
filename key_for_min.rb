5ea65bbab07fcc98359f4a30d994692951a3fb64
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2019-12-21 21:20:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,13 +5,13 @@ def key_for_min_value(name_hash)
   if name_hash == {}
     return nil
   else
-    lowValue = name_hash.first[1]
-    lowKey = name_hash.first[0]
-    name_hash.each{|key, value|
+    lowValue = name_hash.first[1] #set lowest value to hash first value
+    lowKey = name_hash.first[0] #set key to the lowest value to first position in hash
+    name_hash.each{|key, val| #for each key and value do as follows under
       if val < lowValue
         lowValue = val
         lowKey = key
-      end
+      end}
     end
     return lowKey
     
